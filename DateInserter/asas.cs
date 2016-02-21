6f03ac2072ccd5993770ee2931cc28b335ab5b38
jawadefaj <jawadefaj006@gmail.com>
2016-02-21 11:32:34 +0600

    Added files via upload

---
+++ DateInserter/asas.cs
@@ -0,0 +1,16 @@
+﻿using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui;
+using MonoDevelop.Ide.Gui.Content;
+using Mono.TextEditor;
+
+namespace DateInserter
+{
+	public class InsertDateHandler
+	{
+		public InsertDateHandler ()
+		{
+			string stopeer = "Jawad";
+		}
+	}
+}
+
